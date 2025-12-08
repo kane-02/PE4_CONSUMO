b9c8a47fa4b0b9f448d4e5ee59922e0d9f108e03
kane-02 <enbayelon@gmail.com>
2025-12-08 00:11:58 -0600

    informe pdf

--- Modelo_Var.R
+++ Modelo_Var.R
@@ -13,16 +13,17 @@ library(readxl)
 # Extracción clasica de ddatos
 # ------------------------------------------------------------------------------
 
-datos <- read_excel("datos.xlsx", sheet = 2)
+datos <- read_excel("datos.xlsx", sheet = 3)
 
 var1 <- datos[12:403,2]
 var2 <- datos[12:403,3]
 var3 <- datos[12:403,4]
 
+
 mts_data <- ts(cbind(var1, var2, var3), start = c(1993, 1), frequency = 12)
 
 plot.ts(mts_data, main = "Dinámica Multivariada")
-plot.ts(diff(mts_data), main = "Dinámica Multivariada")
+# plot.ts(diff(mts_data), main = "Dinámica Multivariada")
 
 #Prueba de cuantos resagos necesita nuesto model0 VAR
 lag_selection <- VARselect(mts_data, lag.max = 8, type = "const")
@@ -30,7 +31,8 @@ lag_selection$selection
 print(lag_selection)
 
 # Estimación del modelo VAR con la librería
-var_model <- VAR(mts_data, p = 2, type = "const")
+
+var_model <- VAR(mts_data, p = 6, type = "const")
 
 # Muestra las ecuaciones individuales. Explicar R-cuadrado y significancia.
 summary(var_model)
@@ -50,3 +52,7 @@ serial_test
 # C. Normalidad de los residuos
 norm_test <- normality.test(var_model)
 norm_test
+
+
+    
+
